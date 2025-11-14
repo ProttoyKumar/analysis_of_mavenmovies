37cb4a5b50a873fe109b69d4f75b328243c8d772
Prottoy Kumar Prodhan Joy <80445079+ProttoyKumar@users.noreply.github.com>
2025-11-15 02:20:55 +0600

    Add files via upload

--- common_join_types.sql
+++ common_join_types.sql
@@ -37,3 +37,56 @@ FROM
     film_actor ON film.film_id = film_actor.film_id
 GROUP BY film.title
 
+-- Bridging
+-- no common key between film and category table
+-- but both have common key with film_category table
+-- film_category table acts as a bridge between film & category table
+SELECT 
+    film.film_id, film.title, category.name
+FROM
+    film
+Left Join film_category on film.film_id = film_category.film_id
+Left Join category on film_category.category_id = category.category_id
+
+-- Find list of all actors with each film title that they appear in
+SELECT 
+    actor.first_name, actor.last_name, film.title
+FROM
+    actor
+Left Join film_actor on actor.actor_id = film_actor.actor_id
+Left Join film on film_actor.film_id = film.film_id
+
+-- Add multiple conditions
+-- retrieve all films that belong to the Horror category, showing their IDs, titles, and category name.
+SELECT 
+    film.film_id, film.title, category.name as category_name
+FROM
+    film
+        INNER JOIN
+    film_category ON film.film_id = film_category.film_id
+        INNER JOIN
+    category ON category.category_id = film_category.category_id
+WHERE
+    category.name = 'Horror'
+
+-- List distinct titles, descriptions available at store 2
+SELECT DISTINCT
+    film.title, 
+    film.description, 
+	inventory.store_id
+FROM
+    film
+        INNER JOIN
+    inventory ON film.film_id = inventory.film_id
+WHERE
+    inventory.store_id = 2
+
+-- Alternative
+SELECT DISTINCT
+    film.title, film.description, inventory.store_id
+FROM
+    film
+        INNER JOIN
+    inventory ON film.film_id = inventory.film_id
+        AND inventory.store_id = 2
+
