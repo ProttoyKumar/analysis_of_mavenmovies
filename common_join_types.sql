0d129a045b00b221a78e78c72301fa2e1ab4f720
Prottoy Kumar Prodhan Joy <80445079+ProttoyKumar@users.noreply.github.com>
2025-11-20 04:55:39 +0600

    Add files via upload

--- common_join_types.sql
+++ common_join_types.sql
@@ -13,7 +13,7 @@ FROM
     film
         INNER JOIN
     inventory ON film.film_id = inventory.film_id
-    
+
 -- LEFT JOIN
 -- keep all left table data + matching right table data (if any)
 
@@ -90,3 +90,110 @@ FROM
     inventory ON film.film_id = inventory.film_id
         AND inventory.store_id = 2
 
+-- UNION
+-- Combine data from two or more tables, stack rows on top of each other
+-- UNION removes duplicate rows
+-- UNION ALL keeps duplicate rows
+
+-- List all staff and advisor names
+SELECT 
+    'advisor' AS type, advisor.first_name, advisor.last_name
+FROM
+    advisor 
+UNION SELECT 
+    'staff' AS type, staff.first_name, staff.last_name
+FROM
+    staff
+    
+-- Assignment
+-- Q2. 
+-- list every film copy in each store along with the movie’s title, rating, rental price, and replacement cost.
+-- solution:
+SELECT 
+    inventory.inventory_id,
+    inventory.store_id,
+    film.title,
+    film.rating,
+    film.rental_rate,
+    film.replacement_cost
+FROM
+    inventory
+        INNER JOIN
+    film ON film.film_id = inventory.film_id
+       
+-- Q3. 
+-- How many film copies (inventory items) each store has for each movie rating?
+-- Solution:
+SELECT 
+    rating, store_id, COUNT(inventory.film_id) total_items
+FROM
+    inventory
+        INNER JOIN
+    film ON film.film_id = inventory.film_id
+GROUP BY rating , store_id
+
+-- Q4. 
+-- How many films of each category are available in each store?
+-- Also calculate the average and total replacement cost for those films.
+-- Solution:
+SELECT 
+    inventory.store_id,
+    category.name,
+    COUNT(film.film_id) AS total_films,
+    AVG(film.replacement_cost) AS avg_replacement_cost,
+    SUM(film.replacement_cost) AS total_replacement_cost
+FROM
+    film
+        INNER JOIN
+    film_category ON film.film_id = film_category.film_id
+        INNER JOIN
+    inventory ON film_category.film_id = inventory.film_id
+        INNER JOIN
+    category ON film_category.category_id = category.category_id
+GROUP BY category.name, inventory.store_id
+
+-- Q5. List all customer details 
+-- Solution:
+SELECT 
+    CONCAT(customer.first_name,
+            ' ',
+            customer.last_name) AS name,
+	customer.store_id,
+    customer.active,
+    address,
+    city,
+    country
+FROM
+    customer
+        LEFT JOIN
+    address ON address.address_id = customer.address_id
+        LEFT JOIN
+    city ON city.city_id = address.city_id
+        LEFT JOIN
+    country ON country.country_id = city.country_id
+
+-- Q6. 
+-- Find each customer’s total rentals and total payment amount, ranked from the highest spender to the lowest.
+-- Solution:
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS name,
+    COUNT(rental_id) AS total_rentals,
+    SUM(amount) AS sum_of_all_payments
+FROM
+    payment
+        LEFT JOIN
+    customer ON customer.customer_id = payment.customer_id
+GROUP BY name
+ORDER BY sum_of_all_payments DESC
+
+-- Q7. use of union
+-- Solution:
+SELECT 
+    'investor' AS type, first_name, last_name, company_name
+FROM
+    investor 
+UNION SELECT 
+    'advisor' AS type, first_name, last_name, Null
+FROM
+    advisor 
+    
